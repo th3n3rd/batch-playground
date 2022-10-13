2d58a261a4aa5121e9f399834ceb7adb7fcbc4b1
Marco Garofalo <marcogarofalo.personal@gmail.com>
2022-10-13 16:01:07 +0100

    Convert into a multi module project and add an external system application

---
+++ external-system/src/main/java/com/example/externalsystem/ExternalSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.externalsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalSystemApplication.class, args);
+	}
+
+}
---
+++ external-system/src/test/java/com/example/externalsystem/ExternalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.externalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
