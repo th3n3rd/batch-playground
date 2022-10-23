e1674a6275ae07266ec110c3022e58e8633f8599
Marco Garofalo <marcogarofalo.personal@gmail.com>
2022-10-23 15:02:57 +0100

    Separate manager vs worker config

--- app/src/main/java/com/example/batch/payment/importing/scaling/none/ImportTransactionsJobConfig.java
+++ app/src/main/java/com/example/batch/payment/importing/scaling/none/ImportTransactionsJobConfig.java
@@ -8,9 +8,12 @@ import com.example.batch.payment.importing.PersistProcessedTransactions;
 import com.example.batch.payment.importing.StandardiseRawTransaction;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.SyncTaskExecutor;
@@ -21,33 +24,54 @@ import org.springframework.core.task.SyncTaskExecutor;
 public class ImportTransactionsJobConfig {
 
     private final JobBuilderFactory jobs;
-    private final StepBuilderFactory steps;
 
     static final String Name = "import-transactions-scaling-none";
 
     @Bean
-    Job importTransactionsJob(
-        PartitionByDateIntervals partitionByDateIntervals,
-        FetchPaginatedRawTransactions fetchPaginatedRawTransactions,
-        StandardiseRawTransaction standardiseRawTransaction,
-        PersistProcessedTransactions persistProcessedTransactions
-    ) {
-        var worker = steps.get("%s:worker".formatted(Name))
-            .<RawTransactions.Detail, Transaction>chunk(100)
-            .reader(fetchPaginatedRawTransactions)
-            .processor(standardiseRawTransaction)
-            .writer(persistProcessedTransactions)
-            .build();
-
-        var manager = steps.get("%s:manager".formatted(Name))
-            .partitioner(worker.getName(), partitionByDateIntervals)
-            .step(worker)
-            .gridSize(1)
-            .taskExecutor(new SyncTaskExecutor())
-            .build();
-
+    Job importTransactionsJob(@Qualifier("manager") Step manager) {
         return jobs.get(Name)
             .start(manager)
             .build();
+    }
+
+    @Configuration
+    @RequiredArgsConstructor
+    static class ManagerConfig {
+
+        private final StepBuilderFactory steps;
+
+        @Bean
+        public Step manager(
+            PartitionByDateIntervals partitionByDateIntervals,
+            @Qualifier("worker") Step worker
+        ) {
+            return steps.get("%s:manager".formatted(Name))
+                .partitioner(worker.getName(), partitionByDateIntervals)
+                .step(worker)
+                .gridSize(1)
+                .taskExecutor(new SyncTaskExecutor())
+                .build();
+        }
+    }
+
+    @Configuration
+    @RequiredArgsConstructor
+    static class WorkerConfig {
+
+        private final StepBuilderFactory steps;
+
+        @Bean
+        public Step worker(
+            FetchPaginatedRawTransactions fetchPaginatedRawTransactions,
+            StandardiseRawTransaction standardiseRawTransaction,
+            PersistProcessedTransactions persistProcessedTransactions
+        ) {
+            return steps.get("%s:worker".formatted(Name))
+                .<RawTransactions.Detail, Transaction>chunk(100)
+                .reader(fetchPaginatedRawTransactions)
+                .processor(standardiseRawTransaction)
+                .writer(persistProcessedTransactions)
+                .build();
+        }
     }
 }
